03431d3102be75966c3fbc38e5773b58b85274ff
Chris <loganc964@gmail.com>
2026-02-07 00:25:26 -0600

    REV 1

--- 03 Python Scripts/Collatz Sequence.py
+++ 03 Python Scripts/Collatz Sequence.py
@@ -1,5 +1,5 @@
 import sys
- 
+
 
 ## Generate Collatz sequence
 
@@ -42,26 +42,4 @@ class EntryType:
             print("Return number is " + RetVal)
 
 EntryType.input_method()
-## Generate Collatz sequence
-
-def collatz(number):
-
-    if number % 2 == 0:
-        print('EVEN')
-        return 3 * number + 1
-
-    elif number % 2 == 1:
-        print('ODD')
-
-
-input_value = 0
-try:
-    print("input value:")
-    input_value = int(input())
-except ValueError:
-    print("ValueError: Not Integer type")
-    quit()
-
-    return_val = str(collatz(input_value))
-    print("Return number is " + return_val)
 
