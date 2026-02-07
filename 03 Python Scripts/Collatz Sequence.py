e9429bd41c317de2f2f89f508106bec487376fbd
Chris <loganc964@gmail.com>
2026-02-07 00:17:24 -0600

    Updated Revision

--- 03 Python Scripts/Collatz Sequence.py
+++ 03 Python Scripts/Collatz Sequence.py
@@ -1,23 +1,45 @@
+import sys
+
+
 ## Generate Collatz sequence
 
-def collatz(number):
+class CollatzLogic:
+    @staticmethod
+    def collatz_calc(number):
+        if number % 2 == 0:
+            print('EVEN')
+            return 3 * number + 1
 
-    if number % 2 == 0:
-        print('EVEN')
-        return 3 * number + 1
+        elif number % 2 == 1:
+            print('ODD')
 
-    elif number % 2 == 1:
-        print('ODD')
+class EntryType:
+    @staticmethod
+    def input_method():
 
+        input_value = "A"
 
-input_value = 0
-try:
-    print("input value:")
-    input_value = int(input())
-except ValueError:
-    print("ValueError: Not Integer type")
-    quit()
+        while True:
 
-    return_val = str(collatz(input_value))
-    print("Return number is " + return_val)
+            input_value = input().strip()
+            print(input_value)
+            if input_value.upper() == "Q":
+                print("Exiting Program")
+                sys.exit()
+                #quit()
+
+            try:
+                print("Enter 'Q' to Quit Program")
+                print("or input value:")
+
+                input_value = int(input_value)
+
+            except ValueError:
+                print("ValueError: Not Integer type")
+                continue
+
+            RetVal = str(CollatzLogic.collatz_calc(input_value))
+            print("Return number is " + RetVal)
+
+EntryType.input_method()
 
