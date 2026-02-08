f675055a95a32923277270daa31ab61153847bf1
Chris <loganc964@gmail.com>
2026-02-07 18:09:15 -0600

    Including new python file Character Picture Grid

---
+++ Python Scripts/04 Character Picture Grid.py
@@ -0,0 +1,21 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+# Transposing into heart
+for i in range(6):
+    transpose = ""
+
+    for row in grid:
+        transpose = transpose + row[i]
+    
+    print(transpose)
+
+
+    
