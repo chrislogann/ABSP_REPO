38ba658946047526e24052f0399752a83cd01a37
chrislogann <loganc964@gmail.com>
2026-02-14 11:21:27 -0600

    Added python file 09 Selective Copy.py and its Copy Folder

---
+++ 09_Copy_Folder/08 mcb.py
@@ -0,0 +1,33 @@
+#! python3
+# mcb.py - Saves and loads pieces of text to the clipboard.
+# Usage:
+#   python "08 mcb.py" save <keyword>  - Saves clipboard to keyword.
+#   python "08 mcb.py" <keyword>       - Loads keyword to clipboard.
+#   python "08 mcb.py" list            - Loads all keywords to clipboard.
+
+import shelve
+import pyperclip
+import sys
+import os
+
+path = os.getcwd()
+
+mcbShelf = shelve.open(path+'\\08 mcb')
+
+# Save clipboard content
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+    mcbShelf[sys.argv[2]] = pyperclip.paste()
+
+elif len(sys.argv) == 2 and sys.argv[1].lower() == 'delete':
+    mcbShelf.clear()
+elif len(sys.argv) == 3 and sys.argv[1].lower() == 'delete':
+    mcbShelf.pop(sys.argv[2])
+
+elif len(sys.argv) == 2:
+    # List keywords and load content
+    if sys.argv[1].lower() == 'list':
+        pyperclip.copy(str(list(mcbShelf.keys())))
+    elif sys.argv[1] in mcbShelf:
+        pyperclip.copy(mcbShelf[sys.argv[1]])
+
+mcbShelf.close()
